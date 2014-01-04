7916f7aee5748aa8a50ae2a07c5fe3d559b083b4
Ayax50000 <fjlsoto@gmail.com>
2014-01-04 00:59:21 -0600

    user interface and controller done

--- interface.rb
+++ interface.rb
@@ -10,22 +10,21 @@ def  interface
           3   - add Grand Mother
           4   - add Grand Father
           5   - add Woman of Parents Generation
-          6   - see Man of Parents Generation
+          6   - add Man of Parents Generation
           7   - add Woman of your Generation
           8   - add Man of your Generation
-          9   - see Grand Mother
-          10  - see Great Grand Mother
-          11  - see Great Grand Father
-          12  - see Great Grand Parents
-          13  - see Grand Mothers
-          14  - see Grand Fathers
-          15  - see Grand Parents
-          16  - see Women of Parents Generation
-          17  - see Men of Parents Generation
-          18  - see all of Parents Generation
-          19  - see Women of your Generation
-          20  - see Men of your Generation
-          21  - see all of your Generation
+          9   - see Great Grand Mother
+          10  - see Great Grand Father
+          11  - see Great Grand Parents
+          12  - see Grand Mothers
+          13  - see Grand Fathers
+          14  - see Grand Parents
+          15  - see Women of Parents Generation
+          16  - see Men of Parents Generation
+          17  - see all of Parents Generation
+          18  - see Women of your Generation
+          19  - see Men of your Generation
+          20  - see all of your Generation
           21  - find what is a person for you
           22  - see Genealogic Tree
           0   -  exit\n\n"
@@ -43,97 +42,119 @@ end
 def ask_name
   puts "Which is the name of this Person"
   name = gets.chomp
+  yield name
 end
-  input = nil
-  while input != 0
-    system 'clear'
-    interface
-    input = gets.chomp.to_i
-    case input
-      when 1 then go_back { Teacher.all }
-      when 2 then go_back do
-          puts "Please write the name for the new teacher"
-          teacher = Teacher.new(gets.chomp)
-          teacher.add
+
+input = nil
+while input != 0
+  system 'clear'
+  interface
+  input = gets.to_i
+  case input
+    when 1 then go_back do
+          ask_name do |name|
+          great = GreatGrandParent.new(name,2)
+          great.add
         end
-      when 3 then go_back do
-          Teacher.all
-          puts " select the number of the teacher"
-          id_teacher = gets.to_id
-          puts " please select the number of subject"
-          Subject.all
-          Subject.assign_teacher(gets.to_id,id_teacher)
+      end
+    when   2 then go_back do
+          ask_name do |name|
+          great = GreatGrandParent.new(name,1)
+          great.add
         end
-      when 4 then go_back do
-          Subject.all
-          puts "Please select the subject number do you like"
-          Subject.all_teachers(gets.to_id)
+      end
+    when   3 then go_back do
+          ask_name do |name|
+          great = GrandParent.new(name,2)
+          great.add
         end
-      when 5 then go_back do
-          puts "What is the name of the new subject"
-          subject = Subject.new(gets.chomp)
-          subject.add
+      end
+    when   4 then go_back do
+          ask_name do |name|
+          great = GrandParent.new(name,1)
+          great.add
         end
-      when 6 then go_back { Subject.all }
-      when 7 then go_back do
-          puts "Please write the name for the new student"
-          student = Student.new(gets.chomp)
-          student.add
+      end
+    when   5 then go_back do
+          ask_name do |name|
+          great = Parent.new(name,2)
+          great.add
         end
-      when 8 then go_back do
-          Student.all
-          puts "Please select the number of student you want to assign"
-          id_student = gets.to_id
-          Subject.all
-          puts "Please select the subject target"
-          Subject.assign_student(gets.to_id,id_student)
+      end
+    when   6 then go_back do
+          ask_name do |name|
+          great = Parent.new(name,1)
+          great.add
         end
-      when 9 then go_back { Student.all }
-      when 10 then go_back do
-          Student.all
-          puts "Please select the number of student you want to assign"
-          id_student = gets.to_id
-          Subject.student_assignations(id_student)
-          puts "Please select the subject target"
-          subject = gets.to_id
-          puts "What is the score"
-          score = gets.to_id
-          Subject.assign_student(subject,id_student,score)
+      end
+    when   7 then go_back do
+          ask_name do |name|
+          great = EqualRelative.new(name,2)
+          great.add
         end
-      when 11 then go_back do
-          Student.all
-          puts "Please select the number of student you want to see the score"
-          id_student = gets.to_id
-          Subject.student_assignations(id_student)
-          puts "Please select the subject"
-          Subject.student_score(gets.to_id,id_student)
+      end
+    when   8 then go_back do
+          ask_name do |name|
+          great = EqualRelative.new(name,1)
+          great.add
         end
+      end
+    when   9 then go_back do
+          great = GreatGrandParent.new(2)
+          great.all_sex
+        end
+    when   10 then go_back do
+        great = GreatGrandParent.new(1)
+        great.all_sex
+      end
+    when   11 then go_back do
+        great = GreatGrandParent.new(2)
+        great.all
+      end
+    when   12 then go_back do
+        great = GrandParent.new(2)
+        great.all_sex
+    when   13 then go_back do
+        great = GrandParent.new(1)
+        great.all_sex
+      end
+    when   14 then go_back do
+        great = GrandParent.new(1)
+        great.all
+      end
+    when   15 then go_back do
+        great = Parent.new(2)
+        great.all_sex
+      end
+    when   16 then go_back do
+        great = Parent.new(1)
+        great.all_sex
+      end
+    when   17 then go_back do
+        great = Parent.new(1)
+        great.all
+      end
+    when   18 then go_back do
+        great = EqualRelative.new(2)
+        great.all_sex
+      end
+    when   19 then go_back do
+        great = EqualRelative.new(1)
+        great.all_sex
+      end
+    when   20 then go_back do
+        great = EqualRelative.new(1)
+        great.all
+      end
+    when   21 then go_back do
+          ask_name do |name|
+          great = GreatGrandParent.new(name,1)
+          great.search
+        end
+      end
+    when   22 then go_back do
+        GreatGrandParent.tree
+      end
     end
   end
-  system 'clear'
-abue = Parent.new("Jose Mejia",1)
-abue.add
-abue.add
-abue.all
-abue.all_sex
-abue.search
-GreatGrandParent.tree
-abue = GrandParent.new("Jose chavez",1)
-abue.add
-abue.add
-#abue.all
-#abue.all_sex
-abue.search
-GreatGrandParent.tree
-abue = EqualRelative.new("isidro mendoza",1)
-abue.add
-abue.add
-#abue.all
-#abue.all_sex
-#abue.search
-GreatGrandParent.tree
-abue = GreatGrandParent.new("Jose Martinez",1)
-abue.add
-abue.add
-#abue.all
-#abue.all_sex
+system 'clear'
